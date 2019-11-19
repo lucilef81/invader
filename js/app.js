f53ab8d58017c99daa14692a791a679a6d3d687b
Lucilef81 <lucile.franzinetti@gmail.com>
2019-11-19 10:51:38 +0100

    creation grille

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
 var app = {
-  
   init: function() {
     console.log('app init');
+    createGrid();
   },
-
 };
+
+// Création de la grille
+
+var grid = document.createElement('table');
+document.querySelector('#invader').appendChild(grid);
+const MAX_ROW = 8;
+const MAX_COLUMNS = 8;
+
+function createGrid() {
+  for (line = 0; line < MAX_ROW; line++) {
+    var row = document.createElement('tr');
+    grid.appendChild(row);
+    for (col = 0; col < MAX_COLUMNS; col++) {
+      var column = document.createElement('td');
+      column.className = 'cell';
+      row.appendChild(column);
+    }
+  }
+}
 
 document.addEventListener('DOMContentLoaded', app.init);
